6bbbb2f411b599f40a7bc7cb53468742aad50a06
koetjeengdjalanan <martinsambulare@gmail.com>
2025-03-25 14:42:16 +0700

    Refactor Docker Compose setup to remove migration service! Update entry point command to initialize application, migrate database, populate data, and create superuser! Enhance logging for better traceability! 🎉🚀

--- nilakandi/management/commands/entry_point.py
+++ nilakandi/management/commands/entry_point.py
@@ -1,25 +1,32 @@
 import logging
 
-from django.db.models import Model
 from django.core.management.base import BaseCommand
 from django.core.management import call_command
 from django.contrib.auth.models import User
 
 from config.env import env
 
-from nilakandi.models import (
-    Subscription,
-    Services,
-    Operation,
-    Marketplace,
-    VirtualMachine,
-    VirtualMachineCost,
-    ExportHistory,
-)
-
 
 class Command(BaseCommand):
-    help = "Initialize the application | Migrate Database and populate with data"
+    """
+    Command to initialize the application by performing the following steps:
+    1. Log the initialization process.
+    2. Make migrations for the 'nilakandi' app.
+    3. Migrate the database.
+    4. Populate the database with initial data.
+    5. Verify the creation and data count of specific models.
+    6. Create a superuser with credentials from environment variables.
+    7. Log the superuser credentials.
+    8. Start the Django development server.
+
+    Attributes:
+        help (str): Description of the command.
+
+    Methods:
+        handle(self, *args, **options): Executes the command steps.
+    """
+
+    help = "Initializes the application by making migrations, migrating the database, populating initial data, creating a superuser, and starting the server."
 
     def handle(self, *args, **options):
         logging.getLogger("django").info("Initializing application...")
@@ -27,43 +34,40 @@ class Command(BaseCommand):
         logging.getLogger("django.db").info("Migrating database...")
         call_command("migrate", interactive=False)
         logging.getLogger("django.db").info("Populating database...")
-        call_command("populate_db", "--start-date=20210101", interactive=False)
-        models: tuple[str, Model] = (
-            ("Subscription", Subscription),
-            ("Services", Services),
-            ("Operation", Operation),
-            ("Marketplace", Marketplace),
-            ("VirtualMachine", VirtualMachine),
-            ("VirtualMachineCost", VirtualMachineCost),
-            ("ExportHistory", ExportHistory),
-        )
-        for model in models:
-            if model[1].objects.exist():
-                logging.getLogger("django.db").info(msg=f"{model[0]} Creation Success!")
-                logging.getLogger("django.db").info(
-                    msg=f"{model[0]} Data Count: {model[1].objects.count()}"
-                )
-            else:
-                logging.getLogger("django.db").error(msg=f"{model[0]} Creation Failed!")
+        call_command("populate_db", start_date="20250201")
         logging.getLogger("django").info("Creating superuser...")
-        super_user = User.objects.create_superuser(
-            username=env(var="NILAKANDI_SUPER_USER_USERNAME", default="arjuna"),
-            password=env(
+        su_creds: dict[str, str] = {
+            "user_name": env(var="NILAKANDI_SUPER_USER_USERNAME", default="arjuna"),
+            "password": env(
                 var="NILAKANDI_SUPER_USER_PASSWORD", default="arjunamencaricinta"
             ),
-            email=env(
+            "email": env(
                 var="NILAKANDI_SUPER_USER_EMAIL", default="arjuna@nilakandi.local"
             ),
-        )
-        self.stdout.writelines(
-            (
-                self.style.NOTICE(
-                    text="Please take note of this superuser credentials:\n"
-                ),
-                f"UserName: {super_user.username}\n",
-                f"Password: {super_user.password}\n",
-                f"Email   : {super_user.email}\n",
+        }
+        if User.objects.get(username=su_creds["user_name"]):
+            logging.getLogger("django").info(
+                "Superuser already exists. Skipping superuser creation."
             )
-        )
+        else:
+            User.objects.create_superuser(
+                username=su_creds["user_name"],
+                password=su_creds["password"],
+                email=su_creds["email"],
+            )
+            self.stdout.writelines(
+                (
+                    self.style.NOTICE(
+                        text="Please take note of this superuser credentials:\n"
+                    ),
+                    f"UserName: {su_creds['user_name']}\n",
+                    f"Password: {su_creds['password']}\n",
+                    f"Email   : {su_creds['email']}\n",
+                )
+            )
         logging.getLogger("django").info("Application initialized.")
-        self.stdout.write("Done.")
+        try:
+            call_command("runserver", "0.0.0.0:21180")
+        except KeyboardInterrupt:
+            logging.getLogger("django").critical("Application terminated.")
+            exit(1)
