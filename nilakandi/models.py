85cfbe4367972b47f082515c470100598d2a997b
koetjeengdjalanan <martinsambulare@gmail.com>
2025-03-26 15:13:06 +0700

    Add MinIO service to Docker Compose for enhanced storage management! 🎉🚀 Configure environment variables and set up persistent volumes! Improve model structure with ExportReport for detailed billing information! 📊✨ Update .gitignore to include additional environment files! 🛠️

--- nilakandi/models.py
+++ nilakandi/models.py
@@ -1,6 +1,7 @@
 import uuid
 from enum import Enum
 
+from caseutil import to_snake
 from django.contrib.postgres.fields import DateTimeRangeField
 from django.db import models
 
@@ -423,4 +424,165 @@ class ExportHistory(models.Model):
             models.Index(fields=["exec_status", "submitted"]),
             models.Index(fields=["exec_type", "submitted"]),
             models.Index(fields=["exec_type", "exec_status", "submitted"]),
+        ]
+
+
+class ExportReport(models.Model):
+    """
+    Model representing an export report with detailed billing information.
+
+    Attributes:
+        id (UUIDField): Primary key, unique identifier for the report.
+        subscription (ForeignKey): Foreign key to the Subscription model.
+        invoice_section_name (CharField): Name of the invoice section.
+        account_name (CharField): Name of the account.
+        account_owner_id (CharField): ID of the account owner.
+        subscription_name (CharField): Name of the subscription.
+        resource_group (CharField): Name of the resource group.
+        resource_location (CharField): Location of the resource.
+        date (DateField): Date of the report.
+        product_name (CharField): Name of the product.
+        meter_category (CharField): Category of the meter.
+        meter_sub_category (CharField): Sub-category of the meter.
+        meter_id (CharField): ID of the meter.
+        meter_name (CharField): Name of the meter.
+        meter_region (CharField): Region of the meter.
+        unit_of_measure (CharField): Unit of measure for the meter.
+        quantity (FloatField): Quantity measured.
+        effective_price (FloatField): Effective price of the resource.
+        cost_in_billing_currency (FloatField): Cost in the billing currency.
+        cost_center (CharField): Cost center associated with the resource.
+        consumed_service (CharField): Consumed service name.
+        resource_id (CharField): ID of the resource.
+        tags (CharField): Tags associated with the resource.
+        offer_id (CharField): ID of the offer.
+        additional_info (JSONField): Additional information in JSON format.
+        service_info_1 (CharField): Additional service information.
+        service_info_2 (CharField): Additional service information.
+        resource_name (CharField): Name of the resource.
+        reservation_id (CharField): ID of the reservation.
+        reservation_name (CharField): Name of the reservation.
+        unit_price (FloatField): Unit price of the resource.
+        product_order_id (CharField): ID of the product order.
+        product_order_name (CharField): Name of the product order.
+        term (CharField): Term of the resource usage.
+        publisher_type (CharField): Type of the publisher.
+        publisher_name (CharField): Name of the publisher.
+        charge_type (CharField): Type of the charge.
+        frequency (CharField): Frequency of the charge.
+        pricing_model (CharField): Pricing model used.
+        availability_zone (CharField): Availability zone of the resource.
+        billing_account_id (BigIntegerField): ID of the billing account.
+        billing_account_name (CharField): Name of the billing account.
+        billing_currency_code (CharField): Currency code used for billing.
+        billing_period_start_date (DateField): Start date of the billing period.
+        billing_period_end_date (DateField): End date of the billing period.
+        billing_profile_id (BigIntegerField): ID of the billing profile.
+        billing_profile_name (CharField): Name of the billing profile.
+        invoice_section_id (BigIntegerField): ID of the invoice section.
+        is_azure_credit_eligible (BooleanField): Indicates if Azure credit is eligible.
+        part_number (CharField): Part number of the resource.
+        pay_g_price (FloatField): Pay-as-you-go price.
+        plan_name (CharField): Name of the plan.
+        service_family (CharField): Family of the service.
+        cost_allocation_rule_name (CharField): Name of the cost allocation rule.
+        benefit_id (CharField): ID of the benefit.
+        benefit_name (CharField): Name of the benefit.
+        resource_location_normalized (CharField): Normalized resource location.
+        account_id (BigIntegerField): ID of the account.
+
+    Methods:
+        __getattr__(name): Converts CamelCase attribute names to snake_case.
+        __setattr__(name, value): Converts CamelCase attribute names to snake_case and sets the value.
+
+    Meta:
+        unique_together (list): Ensures unique constraint on subscription_id, resource_group, date, and meter_id.
+    """
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    subscription = models.ForeignKey(
+        to=Subscription,
+        to_field="subscription_id",
+        on_delete=models.RESTRICT,
+    )
+    invoice_section_name = models.CharField(null=False)
+    account_name = models.CharField(null=False)
+    account_owner_id = models.CharField(null=False)
+    subscription_name = models.CharField(null=False)
+    resource_group = models.CharField(null=True)
+    resource_location = models.CharField(null=False)
+    date = models.DateField(null=False)
+    product_name = models.CharField(null=False)
+    meter_category = models.CharField(null=False)
+    meter_sub_category = models.CharField(null=True)
+    meter_id = models.CharField(null=False)
+    meter_name = models.CharField(null=False)
+    meter_region = models.CharField(null=True)
+    unit_of_measure = models.CharField(null=False)
+    quantity = models.FloatField(null=False)
+    effective_price = models.FloatField(null=False)
+    cost_in_billing_currency = models.FloatField(null=False)
+    cost_center = models.CharField(null=True)
+    consumed_service = models.CharField(null=True)
+    resource_id = models.CharField(null=True)
+    tags = models.CharField(null=True)
+    offer_id = models.CharField(null=True)
+    additional_info = models.JSONField(default=dict, null=True)
+    service_info_1 = models.CharField(null=True)
+    service_info_2 = models.CharField(null=True)
+    resource_name = models.CharField(null=True)
+    reservation_id = models.CharField(null=True)
+    reservation_name = models.CharField(null=True)
+    unit_price = models.FloatField(null=False)
+    product_order_id = models.CharField(null=True)
+    product_order_name = models.CharField(null=True)
+    term = models.CharField(null=True)
+    publisher_type = models.CharField(null=False)
+    publisher_name = models.CharField(null=False)
+    charge_type = models.CharField(null=False)
+    frequency = models.CharField(null=False)
+    pricing_model = models.CharField(null=False)
+    availability_zone = models.CharField(null=True)
+    billing_account_id = models.BigIntegerField(null=False)
+    billing_account_name = models.CharField(null=False)
+    billing_currency_code = models.CharField(null=False)
+    billing_period_start_date = models.DateField(null=False)
+    billing_period_end_date = models.DateField(null=False)
+    billing_profile_id = models.BigIntegerField(null=False)
+    billing_profile_name = models.CharField(null=False)
+    invoice_section_id = models.BigIntegerField(null=False)
+    is_azure_credit_eligible = models.BooleanField(null=False)
+    part_number = models.CharField(null=False)
+    pay_g_price = models.FloatField(null=False)
+    plan_name = models.CharField(null=False)
+    service_family = models.CharField(null=False)
+    cost_allocation_rule_name = models.CharField(null=True)
+    benefit_id = models.CharField(null=True)
+    benefit_name = models.CharField(null=True)
+    resource_location_normalized = models.CharField(null=True)
+    account_id = models.BigIntegerField(null=False)
+
+    def __getattr__(self, name):
+        # Convert CamelCase to snake_case
+        snake_case_name = to_snake(name)
+        if snake_case_name in self.__dict__:
+            return self.__dict__[snake_case_name]
+        raise AttributeError(
+            f"'{self.__class__.__name__}' object has no attribute '{name}'"
+        )
+
+    def __setattr__(self, name, value):
+        # Convert CamelCase to snake_case
+        snake_case_name = to_snake(name)
+        if snake_case_name in self.__dict__:
+            self.__dict__[snake_case_name] = value
+        else:
+            super().__setattr__(name, value)
+
+    class Meta:
+        unique_together = [
+            "subscription_id",
+            "resource_group",
+            "date",
+            "meter_id",
         ]
